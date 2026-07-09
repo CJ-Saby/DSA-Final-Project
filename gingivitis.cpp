bfe5a2d02147dca942854bd5b568ccdb425ce853
NotATimer <alamreynatheer@gmail.com>
2026-07-09 17:32:45 +0800

    Refactor main function and add list class
    Removed the main function and added a list class for a singly linked list.

--- gingivitis.cpp
+++ gingivitis.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
-int main() {
-  while(true) {
-    std::cout << "FUCK YOU FAG!";
-  }
+class list {
+    private:
+        struct NodeSingly { //singly linked list
+            std::string product_name, product_type, product_ID;
+            int quantity;
+            double unit_cost;
+            NodeSingly *next;
+        };
+        NodeSingly *head = nullptr;
+    public:
 }
