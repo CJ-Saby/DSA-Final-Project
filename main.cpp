e82c84b65c589643a70a238210b83af4edb4969c
BanTheJ <cjohny2007@gmail.com>
2026-07-12 22:47:18 +0800

    A new main file
    Recreated the gingivitis file but renamed it to main.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class list {
+    private:
+        struct NodeSingly { //singly linked list
+            std::string product_name, product_type, product_ID;
+            int quantity;
+            double unit_cost;
+            NodeSingly *next;
+        };
+        NodeSingly *head = nullptr;
+    public:
+}
+
+
+
+
+
+int main() {
+
+
+    return 0;
+}
