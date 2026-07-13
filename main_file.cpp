8a6cb6862946ada07873191bee8a3d57b192fece
CJ-Saby <cjohny2007@gmail.com>
2026-07-13 15:13:52 +0800

    Check if you guys can see the changes.

--- main_file.cpp
+++ main_file.cpp
@@ -2,13 +2,33 @@
 
 class list {
     private:
-        struct NodeSingly { // singly linked list
+
+        struct NodeSingly { // task node blueprint
             std::string taskName, taskDesc;
-            int priority; // ranging from 1 to 10
-            int dateMade[3], dateCompleted[3]; // 0 = Day, 1 = Month, 2 = Year
+            int priority; // ranging from 1 to 10 with 10 being the highest priority
+            int dateMade[3], dateCompleted[3]; // legend of indices: 0 = Day, 1 = Month, 2 = Year
         };
         NodeSingly *head = nullptr;
+
     public:
+
+        void add_task(std::string taskName, std::string taskDesc, int priority) {
+            NodeSingly* newNode = new NodeSingly();
+            newNode->taskName = taskName;
+            newNode->taskDesc = taskDesc;
+            newNode->priority = priority;
+
+            if (head == nullptr) {
+                head = newNode;
+                return;
+            }
+
+            NodeSingly* traverse = head;
+            while (traverse->next != nullptr) {
+
+            }
+        }
+
 };
 
 
