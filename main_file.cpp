18d1dc1dc243a266130f1430489f1a163ae56040
NotATimer <alamreynatheer@gmail.com>
2026-07-15 16:05:54 +0800

    Enhance patientCase structure and search functionality
    Added patientID to patientCase structure and implemented lowercase function for string conversion. Updated search method to handle patient name and ID input.

--- main_file.cpp
+++ main_file.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <cctype>
 
 class list {
     private:
 
         struct patientCase { // task node blueprint
             std::string patientName, caseDesc; // caseDesc is the description of the patient's case
+            std::string patientID; // contains a unique identifier for the patient case
             int priority; // legend: 1 = non emergency, 2 = emergency, 3 = highly urgent emergency
             int dateMade[3], dateCompleted[3]; // legend of indices: 0 = Day, 1 = Month, 2 = Year
+            patientCase* next;
         };
+        
+        std::string lowercase(std::string initial) { // turns a string into lowercase
+            std::string lower = initial;
+            for(int a = 0;a < lower.length();a++) {
+                lower[a] = tolower(lower[a]);
+            }
+            return lower;
+        }
+        
         typedef patientCase* NodePtr;
         patientCase *head = nullptr;
 
@@ -18,24 +30,37 @@ class list {
             // WIP
 
         }
-
-        void add_task(std::string taskName, std::string taskDesc, int priority) {
-            NodeSingly* newNode = new NodeSingly();
-            newNode->taskName = taskName;
-            newNode->taskDesc = taskDesc;
-            newNode->priority = priority;
-
-            if (head == nullptr) {
-                head = newNode;
-                return;
-            }
-
-            NodeSingly* traverse = head;
-            while (traverse->next != nullptr) {
-
+        
+        void update_task() {
+            NodePtr current;
+        }
+        
+        void search() {
+            std::string input;
+            NodePtr current = head;
+            bool found = false;
+            while(true) {
+                std::getline(std::cin, input);
+                std::cout << "Input patient name or ID: ";
+                if(input.length() == 0) {
+                    std::cout << "You didn't input anything\n";
+                    continue;
+                }
+                else {
+                    std::string lower_input = lowercase(input);
+                    if(current == nullptr) {
+                        std::cout << "The Queue is Empty\n";
+                        break;
+                    }
+                    else {
+                        while(current != nullptr) {
+                            std::string lower_name = lowercase(current->patientName);
+                            std::string lower_ID = lowercase(current->patientID);
+                        }
+                    }
+                }
             }
         }
-
 };
 
 
