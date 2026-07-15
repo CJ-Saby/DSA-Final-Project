4373930deee10abe3857945e1b7407f898f8c705
CJ-Saby <cjohny2007@gmail.com>
2026-07-15 23:17:32 +0800

    Finished the sort and add node functions. Only needs to be merged with main branch.

--- main_file.cpp
+++ main_file.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <limits>
 
-class Queue {
+class Queue { // uses doubly linked list
     private:
 
         struct patientCase { // task node blueprint
@@ -9,7 +9,7 @@ class Queue {
             std::string patientID; // contains a unique identifier for the patient case
             int priority; // legend: 1 = non emergency, 2 = emergency, 3 = highly urgent emergency
             int dateMade[3], dateCompleted[3]; // legend of indices: 0 = Day, 1 = Month, 2 = Year
-            patientCase* next;
+            patientCase* next, *prev;
         };
 
         typedef patientCase* NodePtr;
@@ -20,8 +20,31 @@ class Queue {
     
     public:
 
-        void sort_queue(NodePtr caseNode) { // REMOVE THIS?
-            std::cout << "test";
+        void sort_node_in_queue(NodePtr caseNode) { 
+            if (caseNode->prev == nullptr) { // is a check in case the case node is at the front
+                return;
+            } 
+            while (caseNode->prev->priority < caseNode->priority) { // is true when the node to the left has a priority less than the current case node
+                NodePtr prevNode = caseNode->prev; // is the node to the left of the current case node
+                NodePtr prevprevNode = prevNode->prev; // stores the node before the node to the left of the current case node
+                NodePtr nextNode = caseNode->next; // stores the node currently after the case node (to the right of case node)
+                if (prevprevNode == nullptr) { // is for the scenario when current case node is second to the front node
+                    prevNode->next = caseNode->next; // sets the next node of the node to the left of case node to the right of case node
+                    caseNode->next = prevNode; // turns the node formerly to the left to now the node to the right of case node
+                    caseNode->prev = nullptr; // because case node is now the front, its left pointer is now null
+                    prevNode->prev = caseNode; // completes the link of case node and the node formerly to the left which is now the right node
+                    if (nextNode != nullptr) {nextNode->prev = prevNode;} // links the node formerly the left node to the node formerly the right node
+                    front = caseNode; // sets the current case node as the new front 
+                } else {
+                    prevprevNode->next = caseNode; // links the node before the previous node to the case node
+                    prevNode->next = caseNode->next; // links the previous node's next pointer to the node currently after case node
+                    if (prevNode->next == nullptr) {rear = prevNode;} // happens if the swap occurs between the second to the last and the last nodes
+                    caseNode->next = prevNode; // switches the node after case node into the former previous node
+                    prevNode->prev = caseNode; // completes the switch
+                    if (nextNode != nullptr) {nextNode->prev = prevNode;} // links the node formerly after case node to the new next node ONLY if next node exists
+                    caseNode->prev = prevprevNode; // links the case node the the previous node of the formerly previous node
+                }
+            } 
         }
 
         void add_record() {
@@ -52,20 +75,23 @@ class Queue {
             }
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // removes any newline in the buffer
 
-            
             // initialization of the new node
             NodePtr newNode = new patientCase();
             newNode->patientName = name;
             newNode->caseDesc = desc;
             newNode->priority = level;
             newNode->next = nullptr;
+            newNode->prev = nullptr;
 
             if (front == nullptr) { // check if queue is empty
                 front = newNode;
                 rear = newNode;
             } else {
+                newNode->prev = rear;
                 rear->next = newNode;
                 rear = newNode;
+                numItems++; // increments the total items count
+                sort_node_in_queue(newNode);
             }
 
         }
