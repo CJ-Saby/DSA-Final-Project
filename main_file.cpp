a7c5945e34158028e92a1fb6f5550617ecc1f4de
NotATimer <alamreynatheer@gmail.com>
2026-07-15 23:53:28 +0800

    Enhance patient search with linked results
    Added previous pointer to patientCase and searchResults structs. Enhanced search function to store and display multiple found nodes.

--- main_file.cpp
+++ main_file.cpp
@@ -9,7 +9,13 @@ class list {
             std::string patientID; // contains a unique identifier for the patient case
             int priority; // legend: 1 = non emergency, 2 = emergency, 3 = highly urgent emergency
             int dateMade[3], dateCompleted[3]; // legend of indices: 0 = Day, 1 = Month, 2 = Year
-            patientCase* next;
+            patientCase* next, *prev;
+        };
+        
+        struct searchResults {
+            patientCase *foundNode;
+            searchResults *next, *prev;
+            int num;
         };
         
         std::string lowercase(std::string initial) { // turns a string into lowercase
@@ -35,13 +41,16 @@ class list {
             NodePtr current;
         }
         
-        void search() {
+        void search() { //searches all similar nodes
             std::string input;
             NodePtr current = head;
+            searchResults *crest = nullptr, *conductor;
             bool found = false;
-            while(true) {
-                std::getline(std::cin, input);
+            int GotYa = 1;
+            
+            while(true) { //input loop
                 std::cout << "Input patient name or ID: ";
+                std::getline(std::cin, input);
                 if(input.length() == 0) {
                     std::cout << "You didn't input anything\n";
                     continue;
@@ -53,9 +62,35 @@ class list {
                         break;
                     }
                     else {
-                        while(current != nullptr) {
+                        while(current != nullptr) { //searching and recording each found node
                             std::string lower_name = lowercase(current->patientName);
                             std::string lower_ID = lowercase(current->patientID);
+                            if(lower_name.find(lower_input) != std::string::npos || lower_ID.find(lower_input) != std::string::npos) {
+                                if(GotYa == 1) {
+                                    crest = new searchResults;
+                                    conductor = crest;
+                                    conductor->foundNode = current;
+                                    conductor->prev = nullptr;
+                                    conductor->next = nullptr;
+                                    conductor->num = GotYa;
+                                    GotYa++;
+                                }
+                                else {
+                                    conductor->next = new searchResults;
+                                    conductor->next->prev = conductor;
+                                    conductor = conductor->next;
+                                    conductor->next = nullptr;
+                                    conductor->num = GotYa;
+                                    GotYa++;
+                                }
+                            }
+                            current = current->next;
+                        }
+                        while(conductor != nullptr) { //displaying the found nodes
+                            conductor = crest;
+                            std::cout << "[" << conductor->num << "]\n";
+                            std::cout << "Name: " << conductor->foundNode->patientName << std::endl;
+                            std::cout << "ID: " << conductor->foundNode->patientID << std::endl;
                         }
                     }
                 }
