c7deb8981d9b50e75de1057f8c3eea045bf14ed7
NotATimer <alamreynatheer@gmail.com>
2026-07-16 12:51:26 +0800

    Improved memory handling and prevented memory leaks

--- main_file.cpp
+++ main_file.cpp
@@ -69,117 +69,135 @@ class list {
         }
         
         void search() { //searches all similar nodes
-            std::string input;
-            NodePtr current = front;
-            searchResults *crest = nullptr, *conductor; //conductor will traverse like how a conductor moves between train carts, crest is the head/front of the list
-            int GotYa = 1; //just a counter if it got something
+            char choice;
             
-            while(true) { //input loop
-                std::cout << "Input patient name or ID: ";
-                std::getline(std::cin, input);
+            do {
+                std::string input;
+                NodePtr current = front;
+                searchResults *crest = nullptr, *conductor; //conductor will traverse like how a conductor moves between train carts, crest is the head/front of the list
+                int GotYa = 1; //just a counter if it got something
                 
-                if(input.length() == 0) {
-                    std::cout << "You didn't input anything\n";
-                    continue;
-                }
-                else {
-                    std::string lower_input = lowercase(input);
-                    break;
-                }
-            }
-            
-            if(current == nullptr) {
-                std::cout << "The Queue is Empty\n";
-            }
-            else {
-                while(current != nullptr) { //searching and recording each found node
-                    std::string lower_name = lowercase(current->patientName);
-                    std::string lower_ID = lowercase(current->patientID);
-                            
-                    if(lower_name.find(lower_input) != std::string::npos || lower_ID.find(lower_input) != std::string::npos) {
-                        if(GotYa == 1) {
-                            crest = new searchResults;
-                            conductor = crest;
-                            conductor->foundNode = current;
-                            conductor->prev = nullptr;
-                            conductor->next = nullptr;
-                            conductor->num = GotYa;
-                            GotYa++;
-                        }
-                        else {
-                            conductor->next = new searchResults;
-                            conductor->next->prev = conductor;
-                            conductor = conductor->next;
-                            conductor->next = nullptr;
-                            conductor->num = GotYa;
-                            GotYa++;
-                        }
-                    }
-                    current = current->next;
-                }
-                        
-                if(crest == nullptr) {
-                    std::cout << "No matches found\n";
-                }
-                else {
-                    conductor = crest;
-                    while(conductor != nullptr) { //displaying the found nodes
-                        std::cout << "[" << conductor->num << "]\n";
-                        std::cout << "Name: " << conductor->foundNode->patientName << std::endl;
-                        std::cout << "ID: " << conductor->foundNode->patientID << std::endl;
-                        conductor = conductor->next;
-                    }
-                
-                int selection;
-                while(true) {
-                    std::cout << "\nEnter a Number: ";
-                    std::cin >> selection;
-                    if(std::cin.fail() || selection >= GotYa) {
-                        std::cout << "Invalid Input!\n";
-                        std::cin.clear();
-                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                while(true) { //input loop
+                    std::cout << "Input patient name or ID: ";
+                    std::getline(std::cin, input);
+                    
+                    if(input.length() == 0) {
+                        std::cout << "You didn't input anything\n";
                         continue;
                     }
                     else {
-                        conductor = crest;
+                        std::string lower_input = lowercase(input);
                         break;
                     }
                 }
-                int choice;
-                while(conductor != nullptr) {
-                    if(conductor->num == selection) {
-                        while(true) {
-                            std::cout << "\n[1] Edit\n[2] Delete\n[3] Exit\nInput: ";
-                            std::cin >> choice;
-                            if(std::cin.fail()) {
-                                std::cout << "Invalid Input!\n";
-                                std::cin.clear();
-                                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                                continue;
+                
+                if(current == nullptr) {
+                    std::cout << "The Queue is Empty\n";
+                }
+                else {
+                    while(current != nullptr) { //searching and recording each found node
+                        std::string lower_name = lowercase(current->patientName);
+                        std::string lower_ID = lowercase(current->patientID);
+                                
+                        if(lower_name.find(lower_input) != std::string::npos || lower_ID.find(lower_input) != std::string::npos) {
+                            if(GotYa == 1) {
+                                crest = new searchResults;
+                                conductor = crest;
+                                conductor->foundNode = current;
+                                conductor->prev = nullptr;
+                                conductor->next = nullptr;
+                                conductor->num = GotYa;
+                                GotYa++;
                             }
-                            else if(choice < 1 || choice > 3) {
-                                std::cout << "Invalid Input!\n";
-                                continue;
+                            else {
+                                conductor->next = new searchResults;
+                                conductor->next->prev = conductor;
+                                conductor = conductor->next;
+                                conductor->foundNode = current;
+                                conductor->next = nullptr;
+                                conductor->num = GotYa;
+                                GotYa++;
                             }
-                            else break;
                         }
-                        
-                        if(choice == 1) {
-                            update_node(conductor);
-                        }
-                        else if(choice == 2) {
-                            remove_node(conductor);
-                            std::cout << "Node Deleted\n";
-                        }
-                        else if(choice == 3) {
-                            std::cout << "Returning\n";
-                        }
+                        current = current->next;
+                    }
+                            
+                    if(crest == nullptr) {
+                        std::cout << "No matches found\n";
                     }
                     else {
-                        conductor = conductor->next;
+                        conductor = crest;
+                        while(conductor != nullptr) { //displaying the found nodes
+                            std::cout << "[" << conductor->num << "]\n";
+                            std::cout << "Name: " << conductor->foundNode->patientName << std::endl;
+                            std::cout << "ID: " << conductor->foundNode->patientID << std::endl;
+                            conductor = conductor->next;
+                        }
+                    
+                    int selection;
+                    while(true) {
+                        std::cout << "\nEnter a Number: ";
+                        std::cin >> selection;
+                        if(std::cin.fail() || selection >= GotYa) {
+                            std::cout << "Invalid Input!\n";
+                            std::cin.clear();
+                            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                            continue;
+                        }
+                        else {
+                            conductor = crest;
+                            break;
+                        }
                     }
+                    int choice;
+                    while(conductor != nullptr) {
+                        if(conductor->num == selection) {
+                            while(true) {
+                                std::cout << "\n[1] Edit\n[2] Delete\n[3] Exit\nInput: ";
+                                std::cin >> choice;
+                                if(std::cin.fail()) {
+                                    std::cout << "Invalid Input!\n";
+                                    std::cin.clear();
+                                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                                    continue;
+                                }
+                                else if(choice < 1 || choice > 3) {
+                                    std::cout << "Invalid Input!\n";
+                                    continue;
+                                }
+                                else break;
+                            }
+                            
+                            if(choice == 1) {
+                                update_node(conductor);
+                            }
+                            else if(choice == 2) {
+                                remove_node(conductor);
+                                std::cout << "Node Deleted\n";
+                            }
+                            else if(choice == 3) {
+                                std::cout << "Returning\n";
+                            }
+                            break;
+                        }
+                        else {
+                            conductor = conductor->next;
+                        }
+                    }
+                    
+                    searchResults *cleaner;
+                    while(crest != nullptr) {
+                        cleaner = crest;
+                        crest = crest->next;
+                        delete cleaner;
+                    }
+                    
+                    
+                    std::cout << "\nContinue(Enter y if yes, anything else if no): ";
+                    std::cin >> choice;
+                        
                 }
-            }
+            } while(tolower(choice) == 'y');
         }
 };
 
